d8fd772946939134ccf02800f85877325a6547c2
Edgar Morales <emoralesg@buk.cl>
2025-08-31 23:09:18 -0400

    add: true black on nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,18 @@ require("config.vim")
 
 require("config.lazy")
 vim.cmd.colorscheme("catppuccin")
+
+local force_black = {
+  "Normal", "NormalNC", "SignColumn", "LineNr", "CursorLine",
+  "CursorLineNr", "FoldColumn", "StatusLine", "StatusLineNC",
+  "TabLine", "TabLineFill", "WinSeparator", "EndOfBuffer",
+  "NormalFloat", "FloatBorder", "Pmenu", "PmenuSel",
+  "TelescopeNormal", "TelescopeBorder", "TelescopePromptNormal",
+  "TelescopePromptBorder", "TelescopeResultsNormal",
+  "TelescopeResultsBorder", "TelescopePreviewNormal",
+  "TelescopePreviewBorder",
+}
+
+for _, g in ipairs(force_black) do
+  vim.api.nvim_set_hl(0, g, { bg = "#000000" })
+end
