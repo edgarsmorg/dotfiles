88ab9ff747b569e0a0151550920e40fde4c80a6a
Edgar Morales <emoralesg@buk.cl>
2025-09-01 01:32:30 -0400

    add: initial lsp config

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+   "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        "mason-org/mason.nvim",
+        "neovim/nvim-lspconfig",
+    },  },
+  {
+    "neovim/nvim-lspconfig",
+    keys = {
+      { mode="n", "K", vim.lsp.buf.hover, desc = "" },
+      { mode="n", "gd", vim.lsp.buf.definition, desc = "" },
+      { mode={ "n","v" }, "ga", vim.lsp.buf.code_action, desc = "" }
+    },
+    config = function()
+      vim.lsp.config('luals', {})
+      vim.lsp.enable('luals')
+      vim.lsp.config('rubocop', {
+        cmd = { "/home/emoralesg/.rbenv/shims/bundle", "exec", "rubocop", "--lsp"}
+      })
+      vim.lsp.enable('rubocop')
+    end
+  }
+}
