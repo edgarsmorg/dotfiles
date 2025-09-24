a795e7f363d247f9ce0cb74f383a8a4133086635
Edgar Morales <emoralesg@buk.cl>
2025-09-23 23:45:35 -0300

    hola

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,8 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 
+    'DaikyXendo/nvim-material-icon',
+    --'nvim-tree/nvim-web-devicons',
+  },
   opts = { themes = "iceberg_dark" }
 }
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -4,7 +4,8 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
+    --"nvim-tree/nvim-web-devicons",
+    'DaikyXendo/nvim-material-icon',
   },
   lazy=false,
   keys = {
